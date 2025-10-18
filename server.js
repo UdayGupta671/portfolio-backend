6f3ee6abdac81552fdd84ed554adc788ac0ee17a
UdayveerGupta <udayveer671@hotmail.com>
2025-10-17 22:17:25 -0400

    initial commit with server setup and file structure

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import createError from "http-errors";
+import morgan from "morgan";
+import cors from "cors";
+import connectDB from "./config/database.js";
+
+dotenv.config();
+const app = express();
+
+// Connect MongoDB
+connectDB();
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Portfolio Backend API 🚀" });
+});
+
+// Error Handler
+app.use((req, res, next) => {
+  next(createError(404, "Route Not Found"));
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
+});
+
+// Start server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
